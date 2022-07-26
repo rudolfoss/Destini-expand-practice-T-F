1273978a05e9e07ffcce86755d0d25c80076b7ed
rudolfoss <79788182+rudolfoss@users.noreply.github.com>
2022-07-26 18:07:48 +0900

    7/26

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+       
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+     
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    
+    @objc func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+      
+        choice1Button.setTitle(storyBrain.getStoryChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getStoryChoice2(), for: .normal)
+    }
 
 }
 
